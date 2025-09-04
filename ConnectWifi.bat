905274b608943ab7a8dd11d4d8c57934d85cc190
thalhauzair1 <thalhauzair4@gmail.com>
2025-09-03 23:54:49 -0700

    Add SSID variable for easier configuration

--- ConnectWifi.bat
+++ ConnectWifi.bat
@@ -4,13 +4,17 @@
 :: ==========================================
 :: 
 :: INSTRUCTIONS:
-:: 1. Edit WifiProfile.xml file:
-::    - Change "YourSSID" to your WiFi network name
+:: 1. Change the SSID below to your WiFi network name
+:: 2. Edit WifiProfile.xml file:
+::    - Change "YourSSID" to your WiFi network name (same as above)
 ::    - Change "YourPassword" to your WiFi password
-:: 2. Run this script as Administrator
-:: 3. The script will automatically connect to your WiFi
+:: 3. Run this script as Administrator
+:: 4. The script will automatically connect to your WiFi
 ::
 :: ==========================================
+
+:: CHANGE THIS TO YOUR WIFI NETWORK NAME
+set SSID=YourSSID
 
 :: auto-elevate
 NET FILE 1>NUL 2>NUL
@@ -23,6 +27,6 @@ if %errorlevel% NEQ 0 (
 netsh wlan add profile filename="%~dp0WiFiProfile.xml" user=all
 
 :: connect
-netsh wlan connect name="YourSSID" || echo error
+netsh wlan connect name="%SSID%" || echo error
 
 pause
